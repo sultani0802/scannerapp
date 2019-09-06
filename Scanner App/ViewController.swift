d6a4ba13c6461901aad4c73d94167b210558031b
Haamed Sultani <haamedsultani@Haameds-2018-MacBook-Pro.local>
2019-09-05 21:15:02 -0400

    Initial Commit

---
+++ Scanner App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scanner App
+//
+//  Created by Haamed Sultani on Sep/1/19.
+//  Copyright © 2019 Haamed Sultani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
